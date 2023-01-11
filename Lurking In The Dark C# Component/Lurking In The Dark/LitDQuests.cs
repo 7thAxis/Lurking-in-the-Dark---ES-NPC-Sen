78bced2f2e8147d7a9995787624f57ee51f1bc1f
Axell <100167201+7thAxis@users.noreply.github.com>
2023-01-11 16:25:26 -0500

    Add files via upload

---
+++ Lurking In The Dark C# Component/Lurking In The Dark/LitDQuests.cs
@@ -0,0 +1,44 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using HarmonyLib;
+
+namespace LurkingInTheDark
+{
+    internal static class LitDQuests
+    {
+        private static IModHelper Helper { get; set; }
+        public static void ApplyPatch(Harmony harmony, IModHelper helper)
+        {
+            Helper = helper;
+
+            Helper.Events.GameLoop.DayEnding += OnDayEnd;
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SpecialOrder), nameof(SpecialOrder.IsTimedQuest)),
+                postfix: new HarmonyMethod(typeof(LitDQuests), nameof(SpecialOrders_IsTimed_postfix))
+            );
+        }
+        private static void SpecialOrders_IsTimed_postfix(SpecialOrder __instance, ref bool __result)
+        {
+            if (__instance.questKey.Value == "LitD.SpecialOrders.SeedQuest")
+            {
+                __result = false;
+            }
+        }
+        public static void OnDayEnd(object? sender, DayEndingEventArgs e)
+        {
+            if (!Context.IsMainPlayer)
+                return;
+
+            foreach (SpecialOrder o in Game1.player.team.specialOrders)
+            {
+                if (o.questKey.Value.StartsWith("LitD.SpecialOrders.SeedQuest"))
+                {
+                    o.dueDate.Value = Game1.Date.TotalDays + 100;
+                }
+
+            }
+        }
+    }
+}
---
+++ Lurking In The Dark C# Component/Lurking In The Dark/ModEntry.cs
@@ -0,0 +1,22 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using HarmonyLib;
+
+namespace LurkingInTheDark
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        internal static IMonitor ModMonitor { get; set; }
+        /*********
+        ** Public methods
+        *********/
+
+        public override void Entry(IModHelper helper)
+        {
+            var harmony = new Harmony(ModManifest.UniqueID);
+            LitDQuests.ApplyPatch(harmony, Helper);
+        }
+    }
+}
